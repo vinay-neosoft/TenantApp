110acceaa8e38e3be46bdcf72ebbc3b121692fe6
Vinay Bhawsar <vinay.bhawsar@neosofttech.com>
2017-09-20 19:58:19 +0530

    [UPDATE] views file modified

--- tenant/tenant/urls.py
+++ tenant/tenant/urls.py
@@ -29,7 +29,8 @@ router.register(r'questions', QuestionViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', 
+                                namespace='rest_framework')),
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^$', DashboardView.as_view()),
 ]
--- tenant/tenantApp/views.py
+++ tenant/tenantApp/views.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
+"""
+    This file contains DashboardView to get user counts,
+    answers count and questions count
+"""
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.models import User
-
 from tenantApp.models import Question, Answer, Tenant
 
-class DashboardView(View):
 
-	def get(self, request):
-		tenants = Tenant.objects.all()
-		total_users_count = User.objects.all().count()
-		total_answers_count = Answer.objects.all().count()
-		total_questions_count = Question.objects.all().count()
-		return render(request, "dashboard.html", {'total_users_count': total_users_count,
-												  'total_answers_count': total_answers_count,
-												  'total_questions_count': total_questions_count,
-												  'tenants': tenants})
+class DashboardView(View):
+    """
+    DashboardView to get user counts, answers count
+    and questions count
+    """
+
+    def get(self, request):
+        """
+        get method to count all user, answers and questions
+        """
+        tenants = Tenant.objects.all()
+        total_users_count = User.objects.all().count()
+        total_answers_count = Answer.objects.all().count()
+        total_questions_count = Question.objects.all().count()
+        return render(request, "dashboard.html", {'total_users_count': total_users_count,
+                                                  'total_answers_count': total_answers_count,
+                                                  'total_questions_count': total_questions_count,
+                                                  'tenants': tenants})
